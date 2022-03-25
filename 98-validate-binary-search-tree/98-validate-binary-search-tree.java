51641176545ad8acd5b0d1583c44503a5fbad1c4
Pranjal Tripathi <64363696+pranjalvkt@users.noreply.github.com>
2022-03-25 22:35:46 +0530

    Time: 2 ms (21.84%), Space: 44.7 MB (10.73%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,21 +14,46 @@
  * }
  */
 class Solution {
-    Integer prev = null;
-
-    public boolean isValidBST(TreeNode root) {
-        if(root == null) return true;
-
-    boolean left = isValidBST(root.left);
-
-    boolean self = true;
-    if(prev != null && root.val <= prev) {
-      self = false;
-    } else {
-      prev = root.val;
+    public class Pair {
+        boolean isBST = true;
+        Integer min = null;
+        Integer max = null;
     }
     
-    boolean right = isValidBST(root.right);
-    return left && right && self;
+    private Pair isBST(TreeNode root) {
+        if(root == null) return new Pair();
+        
+        Pair lp = isBST(root.left);
+        Pair rp = isBST(root.right);
+        Pair currP = new Pair();
+        if(lp.isBST == true && rp.isBST == true) {
+            if((lp.max == null || (lp.max != null && lp.max < root.val)) && (rp.min == null || (rp.min != null && rp.min > root.val))) {
+                currP.isBST = true;
+            } else {
+                currP.isBST = false;
+            }
+        } else {
+            currP.isBST = false;
+        }
+        
+        if(lp.min != null) {
+            currP.min = Math.min(lp.min, root.val);
+        } else {
+            currP.min = root.val;
+        }
+        
+        if(rp.max != null) {
+            currP.max = Math.max(rp.max, root.val);
+        } else {
+            currP.max = root.val;
+        }
+        
+        return currP;
+    }
+    public boolean isValidBST(TreeNode root) {
+        
+        Pair ans = isBST(root);
+        return ans.isBST;
+        
     }
 }
